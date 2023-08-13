5ce4d7f4a447a6f1c1908495f91d552578ccaf9e
myathtutthu-genie <myathtutthu@geniefintech.com>
2023-08-13 12:07:00 +0630

    feat: setup package

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+
+class HomeController extends Controller
+{
+    function index()
+    {
+       
+    }
+}
