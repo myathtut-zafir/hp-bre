d67e682e3974d45cc36dadd1509874b8cea64b61
myathtut-zafir <aa.htut@gmail.com>
2023-08-27 11:35:27 +0630

    chore: remove comment code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,7 +20,6 @@ class HomeController extends Controller
 
     public function index(Request $request)
     {
-        //        $rules = Rule::all();
         $this->fail = [];
         $this->success = [];
         $this->results = [];
@@ -70,7 +69,7 @@ class HomeController extends Controller
 
     public function validateRule(array $values, ExpressionLanguage $expressionLanguage, string $condition): mixed
     {
-        if (! empty($values)) {
+        if (!empty($values)) {
             $expressionResult = $expressionLanguage->evaluate(
                 $condition,
                 $values
@@ -111,12 +110,12 @@ class HomeController extends Controller
 
         foreach ($rule->rules['rules'] as $key => $value) {
             if (str_contains($value, '..') || $rule['rules']['operator'] === ' in ' || str_contains($value, '[')) {
-                $object->{$key} = ' in'.$value;
+                $object->{$key} = ' in' . $value;
             } else {
                 $object->{$key} = $value;
             }
 
-            $condition .= $key.''.$object->{$key};
+            $condition .= $key . '' . $object->{$key};
             if ($count != $i) {
                 $condition .= $rule->rules['operator'];
             }
@@ -129,7 +128,7 @@ class HomeController extends Controller
 
     public function failRule(mixed $rule, string $condition): void
     {
-        if (! array_key_exists($rule->types->value, $this->fail)) {
+        if (!array_key_exists($rule->types->value, $this->fail)) {
             $this->fail[$rule->types->value] = ['status' => 'Referred', 'rule' => $condition, 'rule_id' => $rule->id, 'types' => $rule->types->value, 'message' => 'Referred'];
         }
 
@@ -138,7 +137,7 @@ class HomeController extends Controller
 
     public function successRule(string $condition, mixed $rule): void
     {
-        if (! array_key_exists($rule->types->value, $this->success)) {
+        if (!array_key_exists($rule->types->value, $this->success)) {
             $this->success[$rule->types->value] = ['status' => 'Pre-Approved', 'rule' => $condition, 'rule_id' => $rule->id, 'types' => $rule->types->value, 'message' => $rule->message];
         }
 
