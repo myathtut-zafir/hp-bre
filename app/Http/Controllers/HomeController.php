7fe9575372f027294f65cced717dc50524768cae
myathtut-zafir <aa.htut@gmail.com>
2023-09-01 22:39:01 +0630

    fix: remove log

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,7 +21,6 @@ class HomeController extends Controller
 
     public function index(Request $request)
     {
-        Log::info('dd', ['dd' => $request->all()]);
         $this->fail = [];
         $this->success = [];
         $this->results = [];
